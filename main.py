04509909227cee1efbef3fd0f87dbc8aed535dad
Timur <timka808090@gmail.com>
2020-11-21 18:21:26 +0300

    Circles

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import random
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.update)
+
+    def paintEvent(self, event):
+        self.qp = QPainter()
+        self.qp.begin(self)
+        self.qp.setBrush(QColor('Yellow'))
+        for i in range(10):
+            a = random.randint(25, 100)
+            self.qp.drawEllipse(random.randint(50, 450),
+                                random.randint(50, 450), a, a)
+        self.qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    exit(app.exec())
